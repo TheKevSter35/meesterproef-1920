cdad6e261767dc22d1b1071778ebc1381c8195ee
qiubee <qiubees@hotmail.com>
2020-05-29 12:39:57 +0200

    add user session & redirect to overview page

--- modules/routes/controllers/inlog.js
+++ modules/routes/controllers/inlog.js
@@ -1,40 +1,40 @@
 let users = [{
-	user: "Bob",
-	password: "12345678",
-	agerange: "60+",
+	user: 'Bob',
+	password: '12345678',
+	agerange: '60+',
 	totalpoints: 100,
 	usedtools: [{
-		toolname: "Fishing quiz",
-		description: "A quiz about phishing mail.",
-		type: "quiz",
+		toolname: 'Fishing quiz',
+		description: 'A quiz about phishing mail.',
+		type: 'quiz',
 		maxpoints: 1400,
 		earnedpoints: 150
 	}, {
-		toolname: "Data Dumpy",
-		description: "See what data you can dump and delete.",
-		type: "game",
+		toolname: 'Data Dumpy',
+		description: 'See what data you can dump and delete.',
+		type: 'game',
 		maxpoints: 2300,
 		earnedpoints: 1120
 	}]
 }]
 
 exports.get = function (req, res) {
-	res.render("pages/login")
+	res.render('pages/login')
 }
 
 exports.process = function (req, res) {
-	const params = req.body
-	if (!params.username && !params.password) {
-		res.redirect("/")
+	const username = req.body.username
+	const password = req.body.password
+	if (!email && !password) {
+		res.redirect('/')
 	}
 	users.map((user) => {
-		if (user.user === params.username && user.password === params.password) {
-			res.render("pages/overview", {
-				user: user.user,
-				totalpoints: user.totalpoints 
-			})
+		if (user.user === username && user.password === password) {
+			req.session.user = username
+			req.session.score = user.totalpoints
+			res.redirect('/overzicht')
 		} else {
-			res.redirect("/")
+			res.redirect('/')
 		}
 	})	
 }
