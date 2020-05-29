9e091ee96e34d684d3dc71b67d47b757d6927593
qiubee <qiubees@hotmail.com>
2020-05-29 13:01:42 +0200

    fix indentation

--- modules/routes/quiz.js
+++ modules/routes/quiz.js
@@ -1,28 +1,26 @@
 module.exports = function (app) {
-  app.get('/quiz-detail', (req, res) => {
-    res.render('pages/quiz-detail')
-  })
-  app.get('/quiz1', (req, res) => {
-    res.render('pages/quiz/quiz1')
-  })
-  app.get('/quiz2', (req, res) => {
-    res.render('pages/quiz/quiz2')
-  })
-  app.get('/quiz3', (req, res) => {
-    res.render('pages/quiz/quiz3')
-  })
-  app.get('/quiz4', (req, res) => {
-    res.render('pages/quiz/quiz4')
-  })
-  app.get('/quiz5', (req, res) => {
-    res.render('pages/quiz/quiz5')
-  })
-  app.get('/quiz6', (req, res) => {
-    res.render('pages/quiz/quiz6')
-  })
-  app.get('/quiz7', (req, res) => {
-    res.render('pages/quiz/quiz3')
-  })
-  
-  
+	app.get('/quiz-detail', (req, res) => {
+		res.render('pages/quiz-detail')
+	})
+	app.get('/quiz1', (req, res) => {
+		res.render('pages/quiz/quiz1')
+	})
+	app.get('/quiz2', (req, res) => {
+		res.render('pages/quiz/quiz2')
+	})
+	app.get('/quiz3', (req, res) => {
+		res.render('pages/quiz/quiz3')
+	})
+	app.get('/quiz4', (req, res) => {
+		res.render('pages/quiz/quiz4')
+	})
+	app.get('/quiz5', (req, res) => {
+		res.render('pages/quiz/quiz5')
+	})
+	app.get('/quiz6', (req, res) => {
+		res.render('pages/quiz/quiz6')
+	})
+	app.get('/quiz7', (req, res) => {
+		res.render('pages/quiz/quiz3')
+	})
 }
