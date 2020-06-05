d13503598f4fbb3a9972b6f16279c17e7899a4b8
qiubee <qiubees@hotmail.com>
2020-06-05 14:22:38 +0200

    fix indentation & use of single quotes

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,34 @@
 // Filter buttons
-let btnLijstWeergave    = document.querySelector('#btnLijstWeergave');
-let btnKaartweergave    = document.querySelector('#btnKaartweergave');
+let btnLijstWeergave = document.querySelector('#btnLijstWeergave')
+let btnKaartweergave = document.querySelector('#btnKaartweergave')
 
 // Views
-let listView    = document.querySelector('.plugins-list');
-let mapView     = document.querySelector('.localfocusvisual');
+let listView = document.querySelector('.plugins-list')
+let mapView = document.querySelector('.localfocusvisual')
 
 // Set this als default
 window.onload = function () {
-    // btnLijstWeergave.classList.add('active');
-    mapView.style.display   = 'none';
+	// btnLijstWeergave.classList.add('active');
+	mapView.style.display = 'none'
 }
 
 btnLijstWeergave.addEventListener('click', function (e) {
-    // Toggle buttons
-    btnKaartweergave.classList.remove('active');
-    btnLijstWeergave.classList.add('active');
+	// Toggle buttons
+	btnKaartweergave.classList.remove('active')
+	btnLijstWeergave.classList.add('active')
 
-    listView.style.display  = 'block';
-    mapView.style.display   = 'none';
-});
+	listView.style.display = 'block'
+	mapView.style.display = 'none'
+})
 
 btnKaartweergave.addEventListener('click', function (e) {
 
-    // Toggle buttons
-    btnLijstWeergave.classList.remove('active');
-    btnKaartweergave.classList.add('active');
+	// Toggle buttons
+	btnLijstWeergave.classList.remove('active')
+	btnKaartweergave.classList.add('active')
 
-    // Toggle content
-    mapView.style.display   = 'block';
-    listView.style.display  = 'none';
+	// Toggle content
+	mapView.style.display = 'block'
+	listView.style.display = 'none'
 
-});
+})
