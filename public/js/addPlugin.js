d71054dd98eb7e69d3bde62b2a262ef787a5430c
randy554 <randy554@hotmail.com>
2020-06-05 14:32:18 +0200

    Eslint

--- public/js/addPlugin.js
+++ public/js/addPlugin.js
@@ -1,19 +1,19 @@
 // Upload image input
-let inputImageUpload    = document.querySelector('#uploadFotoInput');
+let inputImageUpload = document.querySelector('#uploadFotoInput')
 
 // On image upload
 inputImageUpload.addEventListener('input', function () {
 
-    if (input.files && input.files[0]) {
-    var reader = new FileReader();
+	if (input.files && input.files[0]) {
+		var reader = new FileReader()
 
-    reader.onload = function(e) {
-        alert(e.target.result);
-      document.getElementById('blah').src=e.target.result;
-    }
+		reader.onload = function(e) {
+			alert(e.target.result)
+			document.getElementById('blah').src=e.target.result
+		}
 
-    reader.readAsDataURL(input.files[0]); // convert to base64 string
-  }
+		reader.readAsDataURL(input.files[0]) // convert to base64 string
+	}
 
-});
+})
 
