0f5ed7f02766b3166a1e80ba436fc2178f0bf7c0
qiubee <qiubees@hotmail.com>
2020-06-11 15:27:02 +0200

    update "gast" to "jij"

--- modules/routes/controllers/guest.js
+++ modules/routes/controllers/guest.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid')
 
 module.exports = function (req, res) {
 	req.session.userID = uuidv4()
-	req.session.name = 'gast'
+	req.session.name = 'jij'
 	req.session.score = 0
 	req.session.usedtools = []
 	return res.redirect('/overzicht')
