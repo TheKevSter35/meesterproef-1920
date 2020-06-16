e35a7064dc646f98ba9d58c673741ac16038cb92
qiubee <qiubees@hotmail.com>
2020-06-16 15:56:16 +0200

    add scoring & level information to page

--- routes/controllers/dashboard.js
+++ routes/controllers/dashboard.js
@@ -1,14 +1,20 @@
 exports.get = function (req, res) {
+	const { toNextLevel, checkLevel } = require('../../modules/score')
 	if (!req.session.userID) {
 		return res.redirect('/')
 	}
+	const currentLevel = checkLevel(req.session.score)
+	const level = toNextLevel(req.session.score, currentLevel)
 	const user = {
 		name: req.session.name,
 		score: req.session.score,
 		usedTools: req.session.usedtools,
-		level: req.session.level
+		level: req.session.level,
+		help: req.session.help
 	}
 	return res.render('pages/dashboard', {
-		user: user
+		currentLevel: currentLevel,
+		user: user,
+		level: level
 	})
 }
