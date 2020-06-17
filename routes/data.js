002b41290b0fbc1e5f4a551a538bc0087c73c08f
qiubee <qiubees@hotmail.com>
2020-06-17 15:29:46 +0200

    add tool module, add & update tool object

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,19 @@
 module.exports = function (app) {
-	const { review, checkLevel, levelName } = require('../modules/score')
+	const { review, checkLevel, levelName, getTotalScore } = require('../modules/score')
+	const { isUsed, retrieve, updateInfo } = require('../modules/tool')
 	app.get('/data-game', (req, res) => {
+		const usedTools = req.session.usedTools
+		const toolUsed = isUsed('Data enquête', usedTools)
+		if (toolUsed === false) {
+			const tool = {
+				toolname: 'Data enquête',
+				description: 'A survey to ask whether to save all data from the user.',
+				type: 'survey',
+				maxpoints: 100,
+				earnedpoints: 0
+			}
+			req.session.usedTools.push(tool)
+		}
 		const user = {
 			name: req.session.name,
 			score: req.session.score,
@@ -13,9 +26,12 @@ module.exports = function (app) {
 		})
 	})
 	app.post('/data-result', (req, res) => {
+		const tool = retrieve('Data enquête', req.session.usedTools)
+		const toolCollection = req.session.usedTools
 		const answers = req.body
-		const results = review(answers, 100)
-		req.session.score += results.earnedPoints
+		const results = review(answers, tool.maxpoints)
+		updateInfo(tool, 'earnedpoints', results.earnedPoints)
+		req.session.score += getTotalScore(toolCollection)
 		req.session.level = levelName(checkLevel(req.session.score))
 		const user = {
 			name: req.session.name,
